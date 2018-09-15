e2a1104729e7293963b895916b193fb8ce2743ec
Andrey Kashapov <kashapovandrey@gmail.com>
2018-09-15 21:57:32 +0300

    Reusing Backdrop, adding SideDrawer toggle button, adding Hamburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/ReactAux';
 import classes from './Layout.css';
@@ -6,16 +6,34 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
-const layout = (props) => (  
-  <Aux>    
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-console.log(classes.Content);
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
 
-export default layout;
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
