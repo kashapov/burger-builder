473cc755c6280a09abe7bd8db774d70e3d5ecd27
Andrey Kashapov <kashapovandrey@gmail.com>
2018-10-10 20:13:27 +0300

    Navigating Back & Next Page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
     axios.get('https://kashapov-burger-builder.firebaseio.com/ingredients.json')
       .then(response => {
         this.setState({ ingredients: response.data });
@@ -104,28 +105,30 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Andrey Kashapov',
-        address: {
-          street: 'Shevchenko 10',
-          zipCode: '220068',
-          country: 'Belarus'
-        },
-        email: 'kashapovandrey@gmail.com',
-        deliveryMethod: 'fastest'
-      }
-    };
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Andrey Kashapov',
+    //     address: {
+    //       street: 'Shevchenko 10',
+    //       zipCode: '220068',
+    //       country: 'Belarus'
+    //     },
+    //     email: 'kashapovandrey@gmail.com',
+    //     deliveryMethod: 'fastest'
+    //   }
+    // };
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+
+    this.props.history.push('/checkout');
   }
 
   render() {
