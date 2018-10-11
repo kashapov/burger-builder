2e52c2eb4908ac2ebf950dcdb2884444d548eac4
Andrey Kashapov <kashapovandrey@gmail.com>
2018-10-11 20:50:19 +0300

    Order Submission & Passing Data Between Pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -105,33 +105,14 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert('You continue!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Andrey Kashapov',
-    //     address: {
-    //       street: 'Shevchenko 10',
-    //       zipCode: '220068',
-    //       country: 'Belarus'
-    //     },
-    //     email: 'kashapovandrey@gmail.com',
-    //     deliveryMethod: 'fastest'
-    //   }
-    // };
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
+    
     const queryParams = [];
 
     for (let ingr in this.state.ingredients) {
       queryParams.push(encodeURIComponent(ingr) + '=' + encodeURIComponent(this.state.ingredients[ingr]));
     }
+
+    queryParams.push('price=' + this.state.totalPrice);
 
     const queryString = queryParams.join('&');
 
