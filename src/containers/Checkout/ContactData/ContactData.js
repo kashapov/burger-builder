66a980d0507ad97f867ea0bb041682208b336508
Andrey Kashapov <kashapovandrey@gmail.com>
2018-10-17 22:04:54 +0300

    Handling User Input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,6 +82,22 @@ class ContactData extends Component {
       });
   }
 
+  inputChangeHandler = (event, inputIdentifier) => {
+    //console.log(event.target.value, inputIdentifier);
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    this.setState({ orderForm: updatedOrderForm });
+  }
+
   render() {
     const formElementsArray = [];
 
@@ -93,13 +109,14 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form>        
+      <form>
         {formElementsArray.map(formElement => (
           <Input
-            key={formElement.id} 
+            key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value} />
+            value={formElement.config.value}
+            changed={(event) => this.inputChangeHandler(event, formElement.id)} />
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
