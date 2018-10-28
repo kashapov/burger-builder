1f847a8fbebf6983df4ebd5b190860ef8737cf38
Andrey Kashapov <kashapovandrey@gmail.com>
2018-10-28 21:20:23 +0300

    Adjusting Checkout and Contact Data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -61,21 +61,7 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert('You continue!');
-
-    const queryParams = [];
-
-    for (let ingr in this.state.ingredients) {
-      queryParams.push(encodeURIComponent(ingr) + '=' + encodeURIComponent(this.state.ingredients[ingr]));
-    }
-
-    queryParams.push('price=' + this.state.totalPrice);
-
-    const queryString = queryParams.join('&');
-
-    this.props.history.push({
-      pathname: '/checkout',
-      search: '?' + queryString
-    });
+    this.props.history.push('/checkout');
   }
 
   render() {
