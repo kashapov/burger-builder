d342aa7715d7ed46f7a7c431275581eb981aa5a1
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-03 14:05:09 +0300

    Executing Async Code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,13 +26,13 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
     // console.log(this.props);
-    // axios.get('https://kashapov-burger-builder.firebaseio.com/ingredients.json')
-    //   .then(response => {
-    //     this.setState({ ingredients: response.data });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ error: true });
-    //   });
+    axios.get('https://kashapov-burger-builder.firebaseio.com/ingredients.json')
+      .then(response => {
+        this.setState({ ingredients: response.data });
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
   }
 
   updatePurchaseState(ingredients) {
