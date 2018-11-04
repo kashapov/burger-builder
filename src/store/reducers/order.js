7517da5f77c0f52f05a67f55cede3df8ec8765cb
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-04 11:59:05 +0300

    Combining Reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -5,7 +5,7 @@ const initialState = {
   loading: false
 };
 
-cost reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_BURGER_START:
       return {
@@ -17,7 +17,6 @@ cost reducer = (state = initialState, action) => {
         ...action.orderData,
         id: action.orderId
       };
-
       return {
         ...state,
         loading: false,
