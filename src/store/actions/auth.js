a80e66ce9dfb6f0fed116a0f68125dde218136cd
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-08 20:47:02 +0300

    Getting Token from te Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAxHrVBNAKe6_K7-Qh8Ml1v1UWTXgtUgKo', authData)
+    .then(response => {
+      console.log(response);
+      dispatch(authSuccesst(authData.data));
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch(authFail(err));
+    });
   };
 };
