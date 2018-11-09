da7fbb8beef5d616824e093de2e7ae32c8651572
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-09 20:54:21 +0300

    npm i for package-lock

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,11 +1,11 @@
-import * as actionType from '../actions/actionTypes';
+import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../utility';
 
-const iniialState = {
+const initialState = {
   token: null,
   userId: null,
   error: null,
-  loadig: false
+  loading: false
 };
 
 const authStart = (state, action) => {
@@ -15,11 +15,12 @@ const authStart = (state, action) => {
 const authSuccess = (state, action) => {
   return updateObject(state, {
     token: action.idToken,
-    userId: action.user.Id,
+    userId: action.userId,
     error: null,
     loading: false
   });
 };
+
 const authFail = (state, action) => {
   return updateObject(state, {
     error: action.error,
@@ -27,11 +28,11 @@ const authFail = (state, action) => {
   });
 };
 
-const reducer = (state = iniialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionType.AUTH_START: return authStart(state, action);
-    case actionType.AUTH_SUCCESS: return authSuccess(state, action);
-    case actionType.AUTH_FAIL: return authFail(state, action);
+    case actionTypes.AUTH_START: return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL: return authFail(state, action);
     default: return state;
   }
 };
