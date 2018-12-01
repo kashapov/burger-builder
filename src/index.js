fb7549400f66e9b2c3e8be96e4788aa71bbcb616
Andrey Kashapov <kashapovandrey@gmail.com>
2018-12-01 15:19:18 +0300

    Moving the BurgerBuilder side effects to a Saga

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
-import { watchAuth } from "./store/sagas";
+import { watchAuth, watchBurgerBuilder } from "./store/sagas";
 
 const composeEnhancers =
   process.env.NODE_ENV === "development"
@@ -34,6 +34,7 @@ const store = createStore(
 );
 
 sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
 
 const app = (
   <Provider store={store}>
